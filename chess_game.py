24c6bbf07a5429b34868b380041da8069015570c
b.jarkiewicz <b.jarkiewicz@globallogic.com>
2022-05-18 16:03:15 +0200

    Chess game initial completion commit.

--- chess_game.py
+++ chess_game.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from PyQt6.QtCore import QSize, Qt, QPoint
+from PyQt6.QtGui import QPicture, QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
+import sys
 class Board:
     def __init__(self):
         self.grid = [[False for _ in range(8)] for _ in range(8)]
@@ -18,7 +22,10 @@ class Pawn(Piece):
         self.player = player
         self.position = position
         self.moved = False
-        self.path = "/pngs/pawn.png"
+        if player:
+            self.path = "./pngs/pawn.png"
+        else:
+            self.path = "./pngs/b_pawn.png"
     def move(self,changed):
         self.position = changed
         self.moved = True
@@ -30,7 +37,10 @@ class Knight(Piece):
     def __init__(self, player, position):
         self.player = player
         self.position = position
-        self.path = "/pngs/knight.png"
+        if player:
+            self.path = "./pngs/knight.png"
+        else:
+            self.path = "./pngs/b_knight.png"
 
 
     def move(self,changed):
@@ -45,7 +55,10 @@ class Bishop(Piece):
     def __init__(self, player, position):
         self.player = player
         self.position = position
-        self.path = "/pngs/bishop.png"
+        if player:
+            self.path = "./pngs/bishop.png"
+        else:
+            self.path = "./pngs/b_bishop.png"
 
     def move(self, changed):
         self.position = changed
@@ -59,7 +72,10 @@ class Rook(Piece):
         self.player = player
         self.position = position
         self.moved = False
-        self.path = "/pngs/rook.png"
+        if player:
+            self.path = "./pngs/rook.png"
+        else:
+            self.path = "./pngs/b_rook.png"
 
     def move(self, changed):
         self.position = changed
@@ -73,7 +89,10 @@ class Queen(Piece):
     def __init__(self, player, position):
         self.player = player
         self.position = position
-        self.path = "/pngs/queen.png"
+        if player:
+            self.path = "./pngs/queen.png"
+        else:
+            self.path = "./pngs/b_queen.png"
 
     def move(self, changed):
         self.position = changed
@@ -85,7 +104,10 @@ class King(Piece):
         self.player = player
         self.position = position
         self.moved = False
-        self.path = "/pngs/king.png"
+        if player:
+            self.path = "./pngs/king.png"
+        else:
+            self.path = "./pngs/b_king.png"
 
     def move(self, changed):
         self.position = changed
@@ -102,30 +124,117 @@ class Game:
             board.grid[piece.position[0]][piece.position[1]] = True
     def player_move(self):
         pass
-pieces = []
-for i in range(8):
-    pieces.append(Pawn(True, [i, 6]))
-    pieces.append(Pawn(False, [i, 1]))
-pieces.append(Knight(True, [1, 7]))
-pieces.append(Knight(True, [6, 7]))
-pieces.append(Bishop(True, [2, 7]))
-pieces.append(Bishop(True, [5, 7]))
-pieces.append(Rook(True, [0, 7]))
-pieces.append(Rook(True, [7, 7]))
-pieces.append(Queen(True, [3, 7]))
-pieces.append(King(True, [4, 7]))
 
-pieces.append(Knight(False, [1, 0]))
-pieces.append(Knight(False, [6, 0]))
-pieces.append(Bishop(False, [2, 0]))
-pieces.append(Bishop(False, [5, 0]))
-pieces.append(Rook(False, [0, 0]))
-pieces.append(Rook(False, [7, 0]))
-pieces.append(Queen(False, [3, 0]))
-pieces.append(King(False, [4, 0]))
-b = Board()
-g = Game(b,pieces)
-print("ok")
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.setWindowTitle("Chess game")
+        self.setFixedSize(QSize(1000, 750))
+        self.MainDisplay()
+
+
+    def MainDisplay(self):
+        board = Board()
+        pieces = []
+        for i in range(8):
+            pieces.append(Pawn(True, [i, 6]))
+            pieces.append(Pawn(False, [i, 1]))
+        pieces.append(Knight(True, [1, 7]))
+        pieces.append(Knight(True, [6, 7]))
+        pieces.append(Bishop(True, [2, 7]))
+        pieces.append(Bishop(True, [5, 7]))
+        pieces.append(Rook(True, [0, 7]))
+        pieces.append(Rook(True, [7, 7]))
+        pieces.append(Queen(True, [3, 7]))
+        pieces.append(King(True, [4, 7]))
+
+        pieces.append(Knight(False, [1, 0]))
+        pieces.append(Knight(False, [6, 0]))
+        pieces.append(Bishop(False, [2, 0]))
+        pieces.append(Bishop(False, [5, 0]))
+        pieces.append(Rook(False, [0, 0]))
+        pieces.append(Rook(False, [7, 0]))
+        pieces.append(Queen(False, [3, 0]))
+        pieces.append(King(False, [4, 0]))
+        self.game = Game(board,pieces)
+        self.board = QLabel(self)
+        self.board.setGeometry(0, 0, 750, 750)
+        self.board.setPixmap(QPixmap("./pngs/chess_board.PNG").scaled(750,750))
+        self.board.show()
+        self.selected = False
+        self.update()
+
+
+    def update(self):
+        try:
+            self.circle.hide()
+        except:
+            pass
+        self.circle = QLabel(self)
+        self.circle.hide()
+        try:
+            for i in range(len(self.p1)):
+                self.p1[i].hide()
+        except:
+            pass
+        self.p1 = []
+        for piece in self.game.pieces:
+            self.p2 = QLabel(self)
+            self.p2.hide()
+            self.p2.setGeometry(30 + piece.position[0] * 89, 10 + 89 * piece.position[1], 85, 85)
+            self.p2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.p2.setPixmap(QPixmap(piece.path).scaledToHeight(85, Qt.TransformationMode.SmoothTransformation))
+            self.p1.append(self.p2)
+        for i in range(len(self.p1)):
+            self.p1[i].show()
+
+
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.MouseButton.LeftButton:
+            if not self.circle.isHidden():
+                self.circle.hide()
+            self.pos = event.pos()
+            self.rect = [(self.pos.x() - 27) // 90, (self.pos.y() - 10) // 90]
+            self.circle.setGeometry(25 + self.rect[0] * 90, 8 + self.rect[1] * 89, 89, 89)
+            self.circle.setPixmap(
+                QPixmap("./pngs/circle.png").scaledToHeight(89, Qt.TransformationMode.SmoothTransformation))
+            self.circle.show()
+            if not self.selected:
+                for piece in self.game.pieces:
+                    if piece.position == self.rect:
+                        self.selected = True
+                        self.piece_selected = piece
+            else:
+                for ind,piece in enumerate(self.game.pieces):
+                    if self.rect == piece.position:
+                        self.game.pieces.pop(ind)
+                self.selected = False
+                self.piece_selected.move(self.rect)
+                self.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
 
 
 
